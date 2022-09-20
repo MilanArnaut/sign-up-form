3e11638ddf8680a33c446d5c679a8b13740fc1ac
MilanArnaut <milanarnaut02@gmail.com>
2022-09-20 23:33:02 +0200

    Selected input fields and error paragraphs

---
+++ script.js
@@ -0,0 +1,14 @@
+const firstName = document.querySelector("#first_name");
+const lastName = document.querySelector("#last_name");
+const email = document.querySelector("#email");
+const phoneNumber = document.querySelector("#phone_number");
+const password = document.querySelector("#password");
+const confirmPassword = document.querySelector("#confirm_password");
+
+const firstNameError = document.querySelector("#first_name_error");
+const lastNameError = document.querySelector("#last_name_error");
+const emailError = document.querySelector("#email_error");
+const phoneNumberError = document.querySelector("#phone_number_error");
+const passwordError = document.querySelector("#password_error");
+const confirmPasswordError = document.querySelector("#confirm_password_error");
+
